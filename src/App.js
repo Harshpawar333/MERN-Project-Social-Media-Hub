2bfa5ab7cf9c27c7b46822e9fb9976efd4a83885
Harshpawar333 <Harshpawar333@gmail.com>
2021-12-03 01:12:00 +0530

    showpassword

--- src/App.js
+++ src/App.js
@@ -5,24 +5,43 @@ import './App.css'
 import { useState } from 'react'
 
 function App() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [age, setAge] = useState('');
-  const [mobileNumber, setMobileNumber] = useState('');
-  const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [age, setAge] = useState('')
+  const [mobileNumber, setMobileNumber] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
 
-  const [firstNameErr, setFirstNameErr] = useState(false);
-  const [lastNameErr, setLastNameErr] = useState(false);
-  const [emailErr, setEmailErr] = useState(false);
-  const [ageErr, setAgeErr] = useState(false);
-  const [mobileNumberErr, setMobileNumberErr] = useState(false);
-  const [passwordErr, setPasswordErr] = useState(false);
+  const [firstNameErr, setFirstNameErr] = useState(false)
+  const [lastNameErr, setLastNameErr] = useState(false)
+  const [emailErr, setEmailErr] = useState(false)
+  const [ageErr, setAgeErr] = useState(false)
+  const [mobileNumberErr, setMobileNumberErr] = useState(false)
+  const [passwordErr, setPasswordErr] = useState(false)
+  const [passwordInput, setPasswordInput] = useState('password')
+  const [confirmPasswordErr, setConfirmPasswordErr] = useState(false)
+
+  function Toggle() {
+    if (passwordInput === 'password') {
+      setPasswordInput('text')
+    } else {
+      setPasswordInput('password')
+    }
+  }
+  function ResetForm() {
+    setFirstName('')
+    setLastName('')
+    setEmail('')
+    setAge('')
+    setMobileNumber('')
+    setPassword('')
+  }
 
   function handleSubmit() {
     const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-    if (firstName === '') {
-      setFirstNameErr(true);
+    if (firstName !== '') {
+      setFirstNameErr(true)
     } else {
       setFirstNameErr(false)
     }
@@ -32,7 +51,7 @@ function App() {
       setLastNameErr(false)
     }
     if (email.match(mailformat)) {
-      setEmailErr(false);
+      setEmailErr(false)
     } else {
       setEmailErr(true)
     }
@@ -41,129 +60,191 @@ function App() {
     } else {
       setAgeErr(false)
     }
-    if (mobileNumber.length!==10) {
+    if (mobileNumber.length !== 10) {
       setMobileNumberErr(true)
     } else {
       setMobileNumberErr(false)
     }
-    if (password.length <8) {
+    if (password.length < 8) {
       setPasswordErr(true)
     } else {
       setPasswordErr(false)
     }
+    if (password !== confirmPassword) {
+      setConfirmPasswordErr(true)
+    } else {
+      setConfirmPasswordErr(false)
+    }
   }
   return (
+    <body>
     <div className="app">
       <div className="container customContainer text-left">
-        <h1 className="text-center">Signup Form</h1>
-        <div class="py-4 pt-0 row">
-          <label for="inputPassword" class="col-sm-3 col-form-label">
-            First Name
-          </label>
-          <div class="col-sm-9">
-            <input
-              type="text"
-              class="form-control"
-              id="inputPassword"
-              placeholder="First Name"
-              value={firstName}
-              onChange={(event) => setFirstName(event.target.value)}
-            />
-            {firstNameErr && <span className="text-danger text-left posAbsolute" >invalid first name</span>}
+        <h1 className="text-center">Sign-up</h1>
+        <form id="resets">
+          <div class="py-4 pt-0 row">
+            <label for="inputPassword" class="col-sm-3 col-form-label">
+              First Name
+            </label>
+            <div class="col-sm-9">
+              <input
+                type="text"
+                class="form-control"
+                id="inputPassword"
+                placeholder="First Name"
+                value={firstName}
+                onChange={(event) => setFirstName(event.target.value)}
+              />
+              {firstNameErr && (
+                <span className="text-danger text-left posAbsolute">
+                  invalid first name
+                </span>
+              )}
+            </div>
           </div>
-        </div>
-        <div class="py-4 pt-0 row">
-          <label for="inputPassword" class="col-sm-3 col-form-label">
-            Last Name
-          </label>
-          <div class="col-sm-9">
-            <input
-              type="text"
-              class="form-control"
-              id="inputPassword"
-              placeholder="Last Name"
-              value={lastName}
-              onChange={(event) => setLastName(event.target.value)}
-            />
-            {lastNameErr && <span className="text-danger text-left posAbsolute">invalid last name</span>}
+          <div class="py-4 pt-0 row">
+            <label for="inputPassword" class="col-sm-3 col-form-label">
+              Last Name
+            </label>
+            <div class="col-sm-9">
+              <input
+                type="text"
+                class="form-control"
+                id="inputPassword"
+                placeholder="Last Name"
+                value={lastName}
+                onChange={(event) => setLastName(event.target.value)}
+              />
+              {lastNameErr && (
+                <span className="text-danger text-left posAbsolute">
+                  invalid last name
+                </span>
+              )}
+            </div>
           </div>
-        </div>
-        <div class="py-4 pt-0 row">
-          <label for="inputPassword" class="col-sm-3 col-form-label">
-            Email Address
-          </label>
-          <div class="col-sm-9">
-            <input
-              type="text"
-              class="form-control"
-              id="inputPassword"
-              placeholder="email@abc.com"
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-            />
-            {emailErr && <span className="text-danger text-left posAbsolute">invalid email Address</span>}
+          <div class="py-4 pt-0 row">
+            <label for="inputPassword" class="col-sm-3 col-form-label">
+              Email Address
+            </label>
+            <div class="col-sm-9">
+              <input
+                type="text"
+                class="form-control"
+                id="inputPassword"
+                placeholder="email@abc.com"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
+              {emailErr && (
+                <span className="text-danger text-left posAbsolute">
+                  invalid email Address
+                </span>
+              )}
+            </div>
           </div>
-        </div>
-        <div class="py-4 pt-0 row">
-          <label for="inputPassword" class="col-sm-3 col-form-label">
-            Age
-          </label>
-          <div class="col-sm-9">
-            <input
-              type="number"
-              class="form-control"
-              id="inputPassword"
-              placeholder="example: 50 "
-              value={age}
-              onChange={(event) => setAge(event.target.value)}
-            />
-            {ageErr && <span className="text-danger text-left posAbsolute">invalid age</span>}
+          <div class="py-4 pt-0 row">
+            <label for="inputPassword" class="col-sm-3 col-form-label">
+              Age
+            </label>
+            <div class="col-sm-9">
+              <input
+                type="number"
+                class="form-control"
+                id="inputPassword"
+                placeholder="example: 50 "
+                value={age}
+                onChange={(event) => setAge(event.target.value)}
+              />
+              {ageErr && (
+                <span className="text-danger text-left posAbsolute">
+                  invalid age
+                </span>
+              )}
+            </div>
           </div>
-        </div>
-        <div class="py-4 pt-0 row">
-          <label for="inputPassword" class="col-sm-3 col-form-label">
-            Mobile Number
-          </label>
-          <div class="col-sm-9">
-            <input
-              type="number"
-              class="form-control"
-              id="inputPassword"
-              placeholder="1234567890"
-              value={mobileNumber}
-              onChange={(event) => setMobileNumber(event.target.value)}
-            />
-            {mobileNumberErr && <span className="text-danger text-left posAbsolute">invalid Mobile number</span>}
+          <div class="py-4 pt-0 row">
+            <label for="inputPassword" class="col-sm-3 col-form-label">
+              Mobile Number
+            </label>
+            <div class="col-sm-9">
+              <input
+                type="number"
+                class="form-control"
+                id="inputPassword"
+                placeholder="1234567890"
+                value={mobileNumber}
+                onChange={(event) => setMobileNumber(event.target.value)}
+              />
+              {mobileNumberErr && (
+                <span className="text-danger text-left posAbsolute">
+                  invalid Mobile number
+                </span>
+              )}
+            </div>
           </div>
-        </div>
-        <div class="py-4 pt-0 row">
-          <label for="inputPassword" class="col-sm-3 col-form-label">
-            Password
-          </label>
-          <div class="col-sm-9">
-            <input
-              type="password"
-              minlength="8" required
-              class="form-control"
-              id="inputPassword"
-              placeholder="Enter Password"
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-            />
-            {passwordErr && <span className="text-danger text-left posAbsolute">password strength must be greater than 8</span>}
+          <div class="py-4 pt-0 row">
+            <label for="inputPassword" class="col-sm-3 col-form-label">
+              Password
+            </label>
+            <div class="col-sm-9">
+              <input
+                type={passwordInput}
+                minlength="8"
+                required
+                class="form-control"
+                id="passId"
+                placeholder="Enter Password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+              <input type="checkbox" onClick={Toggle} />
+              <b1> Show Password</b1>
+              {passwordErr && (
+                <span className="text-danger text-left posAbsolute">
+                  password strength must be greater than 8
+                </span>
+              )}
+            </div>
           </div>
-        </div>
-        <div className="d-flex justify-content-center">
-        <button
-          type="button"
-          className="btn btn-primary"
-          onClick={handleSubmit}
-        >
-          Signup
-        </button>
-        </div>
+          <div class="py-4 pt-0 row">
+            <label for="inputPassword" class="col-sm-3 col-form-label">
+              Confirm Password
+            </label>
+            <div class="col-sm-9">
+              <input
+                type={passwordInput}
+                minlength="8"
+                required
+                class="form-control"
+                id="passId"
+                placeholder="Confirm Password"
+                value={confirmPassword}
+                onChange={(event) => setConfirmPassword(event.target.value)}
+              />
+              {confirmPasswordErr && (
+                <span className="text-danger text-left posAbsolute">
+                  Entered password must be same as above
+                </span>
+              )}
+            </div>
+          </div>
+
+          <div className="d-flex justify-content-center">
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={handleSubmit}
+            >
+              Signup
+            </button>
+            <button type="button" className="btn btn-link" onClick={ResetForm}>
+              Reset
+            </button>
+          </div>
+        </form>
       </div>
     </div>
+    </body>
   )
 }
 
